229522f648e95af10465a777a4259bddfae75199
podule <119zs-tpk@mail.ru>
2019-10-03 18:09:06 +0400

    add class to check homework_2

--- Main.java
+++ Main.java
@@ -1,99 +1,120 @@
-package galia.lessons_1;
+package lessons_2;
+
 
 public class Main {
 
-    // 1. Создать пустой проект в IntelliJ IDEA и прописать метод main();
     public static void main(String[] args) {
-
-        //    2. Создать переменные всех пройденных типов данных, и инициализировать их значения;
-        
-        byte b = 1;
-        short sh = 2;
-        char c = 'a';
-        int i = 3;
-        long l = 4L;
-        float f = 5F;
-        double d = 6.0;
-        boolean bl = true;
-        String s = "Ссылочный тип";
-
-        //3. Написать метод, вычисляющий выражение a * (b + (c / d)) и возвращающий результат, где a, b, c, d – входные параметры этого метода;
-        int result_1 = calculateExpression(2, 2, 6, 2);
-        System.out.println(result_1);
-
-        //4. Написать метод, принимающий на вход два числа, и проверяющий, что их сумма лежит в пределах от 10 до 20 (включительно),
-        // если да – вернуть true, в противном случае – false;
-        boolean is_result_2 = calculateSum(5, 16);
-        System.out.println(is_result_2);
-
-        //5. Написать метод, которому в качестве параметра передается целое число, метод должен напечатать в консоль
-        // положительное число передали или отрицательное (Замечание: ноль считаем положительным числом.);
-        printWhatNumber(0);
-
-        //6. Написать метод, которому в качестве параметра передается целое число, метод должен вернуть true, если число отрицательное;
-        boolean is_result_3 = isWhatNumber(-5);
-        System.out.println(is_result_3);
-
-        //7. Написать метод, которому в качестве параметра передается строка, обозначающая имя,
-        // метод должен вывести в консоль сообщение «Привет, указанное_имя!»;
-        printHello("Galia");
-
-        //8. *Написать метод, который определяет, является ли год високосным, и выводит сообщение в консоль.
-        // Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
-        printIsLeapYear(2000);
-
-        //9. *Не набирая код в IDE, ответьте на следующий вопрос. Есть два метода:
-        //    void myMethod(int a, String b) {}
-        //    void myMethod(String b, int a) {}
-        //
-        //    Это две разных сигнатуры одного метода или один и тот же метод?
-
-        // Ответ - это два разных метода, показан пример перезагрузки метода - если метод имеет разные параметры аргументов,
-        // то методы  считаются разными.
-
-
-    }
-
-    private static void printIsLeapYear(int i) {
-        String s;
-        if( (i%4 == 0 && !(i%100 == 0)) || (i%400 == 0) ){
-            s = "високосным";
-        }else {
-            s = "не високосным";
+        //1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
+        // С помощью цикла и условия заменить 0 на 1, 1 на 0;
+        int[] nums_1 = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for(int i = 0; i < nums_1.length; i++){
+            nums_1[i] = (nums_1[i]== 0) ? 1 : 0;
+            System.out.print(nums_1[i] + " ");
         }
 
-        System.out.println("Год является " + s);
-    }
+        System.out.println("");
 
-    private static void printHello(String name) {
-        System.out.println("Привет, " + name + "!");
-    }
-
-    private static boolean isWhatNumber(int a) {
-
-        return (a < 0) ? true : false;
-
-    }
-
-    private static void printWhatNumber(int a) {
-        String s;
-        if(a >= 0){
-            s = "положительное число";
-        }else{
-            s = "отрицательное число";
+        //2. Задать пустой целочисленный массив размером 8. С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
+        int[] nums_2 = new int[8];
+        for (int i = 0; i < nums_2.length; i++){
+            nums_2[i] = i*3;
+            System.out.print(nums_2[i] + " ");
         }
 
-        System.out.println("Передано " + s);
+        System.out.println("");
+
+        //3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ], пройти по нему циклом, и числа, меньшие 6, умножить на 2;
+        int[] nums_3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        int a = 0;
+        while (a < nums_3.length){
+            if(nums_3[a] < 6){
+                nums_3[a] *= 2;
+            }
+            System.out.print(nums_3[a] + " ");
+            a++;
+        }
+
+        System.out.println("");
+
+        //4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
+        // и с помощью цикла(-ов) заполнить его диагональные элементы единицами;
+        int[][] nums_4 = new int[4][4];
+        for (int i = 0; i < nums_4.length; i++){
+            for (int j = 0; j < nums_4[i].length; j++){
+                nums_4[i][j] = (i == j) ? 1 : 0;
+
+                System.out.print(nums_4[i][j] + " ");
+            }
+            System.out.println("");
+        }
+
+        //5. Задать одномерный массив и найти в нем минимальный и максимальный элементы (без помощи интернета);
+        int[] nums_5 = {5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        int min = nums_5[0];
+        int max = nums_5[0];
+        for(int i : nums_5){
+            min = (i < min) ? i : min;
+            max = (i > max) ? i : max;
+        }
+        System.out.println("min - " + min + " max - " + max);
+
+        //6. Написать метод, в который передается не пустой одномерный целочисленный массив, метод должен вернуть true,
+        // если в массиве есть место, в котором сумма левой и правой части массива равны.
+        // Примеры: checkBalance([1, 1, 1, || 2, 1]) → true, checkBalance ([2, 1, 1, 2, 1]) → false,
+        // checkBalance ([10, || 10]) → true, граница показана символами ||, эти символы в массив не входят;
+        int[] nums_6_1 = {5, 3, 2, 6, 4};
+        int[] nums_6_2 = {1, 1, 1, 2, 1};
+        int[] nums_6_3 = {2, 1, 1, 2, 1};
+        boolean b = checkBalance(nums_6_1);
+        boolean c = checkBalance(nums_6_2);
+        boolean d = checkBalance(nums_6_3);
+        System.out.println(b + " " + c + " " + d);
+
+        //7. *Написать метод, которому на вход подается одномерный массив и число n (может быть положительным или отрицательным),
+        // при этом метод должен сместить все элементы массива на n позиций. Нельзя пользоваться вспомогательными массивами.
+        displaceArr(nums_5, 3);
+        System.out.println(" ");
+        for(int i : nums_5){
+            System.out.print(i + " ");
+        }
+
     }
 
-    private static boolean calculateSum(int a, int b) {
-        int c = a + b;
-        return ( c >= 10 && c <= 20 ) ? true : false;
+
+
+    private static void displaceArr(int[] arr, int a) {
+        int b;
+        int j = (a < 0) ?  arr.length + a : a;
+        for (int i = 0; i < j; i++){
+            b = arr[0];
+
+            for(int y = 0; y < arr.length; y++){
+                arr[y] = (y < arr.length-1) ? arr[y+1] : b;
+            }
+
+            //{5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1}; входной массив
+            // {11, 4, 5, 2, 4, 8, 9, 1, 5, 3, 2};  n = 3
+            // {8, 9, 1, 5, 3, 2, 11, 4, 5, 2, 4}; n = -3
+        }
     }
 
-    private static int calculateExpression(int a, int b, int c, int d) {
+    private static boolean checkBalance(int[] arr) {
+        int summ = 0;
+        for(int i : arr){
+            summ += i;
+        }
 
-        return a * (b + (c / d));
+        int bitArr = 0;
+        for(int i : arr){
+            bitArr += i;
+            if((summ - bitArr) == bitArr){
+                return true;
+            }
+        }
+
+        return false;
     }
 
 }
+
+
